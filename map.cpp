6f48ed68496d7aeb737b23ac5b1d6cf4fe1ff283
mahdizynali <zeinali.mahdy@gmail.com>
2023-05-20 13:10:18 +0330

    begin route creator

--- map.cpp
+++ map.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <vector>
 using namespace std;
+using namespace cv;
 
+vector <int> roadsPoint;
 
-int main() {
-    cv::Mat test;
-    test = cv::imread("/home/maximum/Desktop/Iran-Map-Route-Planning/map.png");
-    cv::imshow("test", test);
-    cv::waitKey(0);
+void drawRoad(){
 
+}
+
+void CallBackFunc(int event, int x, int y, int flags, void* userdata){
+
+    if  ( event == EVENT_LBUTTONDOWN ){
+         cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+    }
+    else if  ( event == EVENT_RBUTTONDOWN ){
+         cout << "Right button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+    }
+    else if  ( event == EVENT_MBUTTONDOWN ){
+         cout << "Middle button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    Mat img = imread("/home/maximum/Desktop/Iran-Map-Route-Planning/map.png");
+
+    if ( img.empty() ) { 
+        cout << "Error loading the image" << endl;
+        return -1; 
+    }
+    
+    setMouseCallback("My Window", CallBackFunc, NULL);
+    imshow("My Window", img);
+    waitKey(0);
     return 0;
 }
+
